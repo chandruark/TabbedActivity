5b4b072e7d340924339bb16efc6ee12cd0def91d
Chandru <kchandruk7@gmail.com>
2017-08-22 20:10:10 +0530

    Add files via upload

---
+++ Latest.java
@@ -0,0 +1,23 @@
+package com.example.chandru.myapplication;
+
+/**
+ * Created by chandru on 8/22/2017.
+ */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Latest extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.latest, container, false);
+
+        return rootView;
+    }
+}
---
+++ Top.java
@@ -0,0 +1,22 @@
+package com.example.chandru.myapplication;
+
+/**
+ * Created by chandru on 8/22/2017.
+ */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Top extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.top, container, false);
+
+        return rootView;
+    }
+}
